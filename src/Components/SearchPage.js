da59030ec09280653ce3fb69868cbce7be93912c
ashmeetsehgal <sglashmeet@gmail.com>
2018-02-22 03:18:03 +0530

    type-along search

--- src/Components/SearchPage.js
+++ src/Components/SearchPage.js
@@ -4,6 +4,12 @@ import PlanetCard from './PlanetCard';
 import { updateSearchResultsAction } from '../Actions/searchAction';
 
 class SearchPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchString: '',
+    };
+  }
   componentDidMount() {
     fetch('https://swapi.co/api/planets/?format=json')
       .then(res => res.json())
@@ -15,6 +21,22 @@ class SearchPage extends Component {
         });
       });
   }
+
+  updateResults = (e) => {
+    this.setState({
+      searchString: e.target.value,
+    }, () => {
+      const { searchString } = this.state;
+      fetch(`https://swapi.co/api/planets/?search=${searchString}`)
+        .then(res => res.json()).then((data) => {
+          this.props.updateSearchResultsAction({
+            results: data.results,
+            prevUrl: data.previous,
+            nextUrl: data.next,
+          });
+        });
+    });
+  }
   renderPlanetCard = (results) => {
     const planetCards = results.map(i => (
       <PlanetCard key={i.name} planetDetails={i} />
@@ -23,10 +45,19 @@ class SearchPage extends Component {
   }
 
   render() {
-    const { results, prevUrl, nextUrl } = this.props;
+    const { searchString } = this.state;
+    const {
+      results,
+      prevUrl,
+      nextUrl,
+    } = this.props;
     return (
       <div>
-        <input type='text' />
+        <input
+          type='text'
+          value={searchString}
+          onChange={this.updateResults}
+        />
         {this.renderPlanetCard(results)}
       </div>
     );
@@ -37,6 +68,7 @@ const mapStateToProps = state => ({
   results: state.searchReducer.results,
   prevUrl: state.searchReducer.prevUrl,
   nextUrl: state.searchReducer.nextUrl,
+  searchString: state.searchReducer.searchString,
 });
 const mapDispatchToProps = dispatch => ({
   updateSearchResultsAction: props => dispatch(updateSearchResultsAction(props)),
